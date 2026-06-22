6f42e8f20ced90ead646e2cbf993accdf8a0ad48
nic-brown <crazyfurymrx@gmail.com>
2026-06-22 18:25:16 +0300

    storage class & calorie limit persist

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 class CalorieTracker {
   constructor() {
-    this._calorieLimit = 2000;
+    this._calorieLimit = Storage.getCalorieLimit();
     this._totalCalories = 0;
     this._meals = [];
     this._workouts = [];
@@ -51,6 +51,7 @@ class CalorieTracker {
   }
   setLimit(newLimit) {
     this._calorieLimit = newLimit;
+    Storage.setCalorieLimit(newLimit);
     this._displayCaloriesLimit();
     this._render();
   }
@@ -161,6 +162,21 @@ class Workout {
     this.id = Math.random().toString(16).slice(2);
     this.name = name;
     this.calories = calories;
+  }
+}
+
+class Storage {
+  static getCalorieLimit(defaultLimit = 2000) {
+    let calorieLimit;
+    if (localStorage.getItem('calorieLimit') === null) {
+      calorieLimit = defaultLimit;
+    } else {
+      calorieLimit = +localStorage.getItem('calorieLimit');
+    }
+    return calorieLimit;
+  }
+  static setCalorieLimit(calorieLimit) {
+    localStorage.setItem('calorieLimit', calorieLimit);
   }
 }
 
